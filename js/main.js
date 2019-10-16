2777270d291876aa43d53699a79648094e5af29b
andrea9llo <andrea.novello93@gmail.com>
2019-10-16 15:01:38 +0200

    creazione password

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// chiedo nome utente
+var nameUtente = prompt("Nome");
+// console.log(nameUtente);
+
+// stringa di saluto dopo inserimento nome
+var saluto = "ciao " + nameUtente;
+// console.log(saluto);
+
+// chiedo cognome utente
+var surname = prompt("Cognome");
+// console.log(surname);
+
+// chiedo colore preferito utente
+var color = prompt("Colore preferito");
+// console.log(color);
+
+// creazione password
+var password = nameUtente + surname + color + 19;
+// console.log(password);
+
+// stringa dopo inserimento cognome
+var creaPassword = saluto + " " + surname +", la tua password è: ";
+// console.log(creaPassword);
+
+// stringa password creata
+var passwordGene = creaPassword + password;
+// console.log(passwordGene);
